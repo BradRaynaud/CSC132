9a298a1ffcadb3703ffa515c6b011787468e187e
BradRaynaud <bradraynaud@gmail.com>
2017-01-04 15:10:41 -0600

    added break functions

---
+++ Class_Work.py
@@ -0,0 +1,15 @@
+#Class work
+# by: Brad Raynaud
+
+def end():
+    exit()
+
+first_name = "Brad"
+last_name = "Raynaud"
+print first_name
+print last_name
+bio = """I am a wonderful human being
+ capable of many wonderful things"""
+print bio
+
+#Mathew This is how github works
