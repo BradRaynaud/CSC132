4ef45ca7ffcf15f07223290663851954d943402f
BradRaynaud <bradraynaud@gmail.com>
2017-01-12 13:45:45 -0600

    added break functions

--- How_Many_Zeros.py
+++ How_Many_Zeros.py
@@ -1,10 +1,15 @@
+###########################################################################################
+# Name: Brad Raynaud
+# Date: January 12, 2017
+# Description: This program calculates the number of zeros written from one to one million.
+###########################################################################################
 n = 1
-zeros = 0
+zeros = 0 # Initializes the zero counter
 
 while n <= 1000000:
-    num = n
-    num = str(num)
-    zeros += num.count("0")
-    n += 1
+    num = n #sets num
+    num = str(num)  # Sets converts num to a string
+    zeros += num.count("0") # Counts the zeros in num
+    n += 1      # Increment n
 
-print zeros
+print zeros     # Print Result
