6dc773ad82be694a92af261f28f94ea8ac3ccb8a
BradRaynaud <bradraynaud@gmail.com>
2017-02-10 01:25:15 -0600

    Class Game

--- Classwork/The_Game.py
+++ Classwork/The_Game.py
@@ -1,16 +1,38 @@
-
+########################################################
+# Name: Brad Raynaud
+# Date: 2/10/2017
+# Description: This program is a game where the objective is to escape the house
+########################################################
+# Change Log(list of improvements)
+# Added the verb usage along with usage text for every item in the game
+# created a second floor labeled attic with a piece of cake and a puzzle box inside it
+# Created a special case for the puzzle box usage so that the puzzle box could only be opened if I had the key in my inventory
+# Upon opening of the box the first key was removed and the window_key was added
+# Using that key you are able to use the go command on the window
+#
+# I was unable to implement a case where the window returned a response "The window is locked"
+# Commands in order to complete game: 1) take key 2) go east 3) go stairs 4) use puzzle_box 5) go stairs 6) go south
+#  7) go window
+#
+#
 
 # Blueprint for a room
 class Room(object):
     # The constructor
     def __init__(self, name):
+        # rooms have a name, exits (e.g., south), exit locations
+        # (e.g., to the south is room n), items (e.g., table), item
+        # descriptions (for each item), and grabbables (things that
+        # can be taken into inventory)
         self.name = name
         self.exits = []
         self.exitLocations = []
         self.items = []
         self.itemDescriptions = []
         self.grabbables = []
+        self.usage = []
 
+    # getters and setters for instance variables
     @property
     def name(self):
         return self._name
@@ -51,6 +73,14 @@ class Room(object):
     def itemDescriptions(self, value):
         self._itemDescriptions = value
 
+    @property # getter for usage verb
+    def usage(self):
+        return self._usage
+
+    @usage.setter # setter for usage verb
+    def usage(self, value):
+        self._usage = value
+
     @property
     def grabbables(self):
         return self._grabbables
@@ -63,10 +93,10 @@ class Room(object):
         self._exits.append(exit)
         self._exitLocations.append(room)
 
-    def addItem(self, item, desc):
+    def addItem(self, item, desc, usage): # added usage to addItem
         self._items.append(item)
         self._itemDescriptions.append(desc)
-
+        self._usage.append(usage) # appends arg(usage) to the usage list for item
     def addGrabbable(self,item):
         self._grabbables.append(item)
 
@@ -74,7 +104,7 @@ class Room(object):
         self.grabbables.remove(item)
 
     def __str__(self):
-        s = "You are in{}.\n".format(self.name)
+        s = "You are in {}.\n".format(self.name)
 
         s += "You see:  "
         for item in self.items:
@@ -99,32 +129,32 @@ def createRooms():
     r1.addExit("east", r2)
     r1.addExit("south", r3)
     r1.addGrabbable("key")
-    r1.addItem("chair", "It is made of wicker and no one is sitting on it.")
-    r1.addItem("table", "it is made of oak. A golden key rests on it")
+    r1.addItem("chair", "It is made of wicker and no one is sitting on it.", "I could sit in this, but I won't")
+    r1.addItem("table", "it is made of oak. A golden key rests on it", "I can't use this")
 
     r2.addExit("stairs", r5)
     r2.addExit("west", r1)
     r2.addExit("south", r4)
-    r2.addItem("rug", "It is nice and Indian. It also needs to be vacuumed")
-    r2.addItem("fireplace", "It is full of ashes.")
+    r2.addItem("rug", "It is nice and Indian. It also needs to be vacuumed", "I can't use this, too dusty")
+    r2.addItem("fireplace", "It is full of ashes.", "Don't want to burn the house down")
 
     r3.addExit("north", r1)
     r3.addExit("east", r4)
     r3.addGrabbable("book")
-    r3.addItem("bookshelves", "They are empty. Go figure.")
-    r3.addItem("statue", "There is nothing special about it")
-    r3.addItem("desk","The statue is resting on it. So is a book.")
+    r3.addItem("bookshelves", "They are empty. Go figure.", "Books eww")
+    r3.addItem("statue", "There is nothing special about it", "Nothing happens")
+    r3.addItem("desk","The statue is resting on it. So is a book.", "I could study, But I won't")
 
     r4.addExit("north", r2)
     r4.addExit("west", r3)
-    r4.addExit("south", None)  # DEATH
+    r4.addExit("window", None)  # DEATH
     r4.addGrabbable("6-pack")
-    r4.addItem("Brew_Rig", "Gourd is brewing some sort of oatmeal stout on the rig. A 6-pack is resting beside it.")
+    r4.addItem("Brew_Rig", "Gourd is brewing some sort of oatmeal stout on the rig. A 6-pack is resting beside it.", "Better leaving brewing to Gourd")
 
     r5.addExit("stairs", r2)  # Adds an exit to the attic
-    r5.addItem("cake", "It looks tasty")  # creates item: cake
-    r5.addItem("puzzle_box", "Looks intricate")  # Creates item: puzzle_box
-
+    r5.addItem("cake", "It looks tasty", "Made of chocolate, but tastes like lies")  # creates item: cake
+    r5.addItem("puzzle_box", "Looks intricate but it does have a keyhole", "The key from downstairs might unlock this")  # Creates item: puzzle_box
+    # creates a base usage case if player does not have key in inventory
     currentRoom = r1
 
 # displays an appropriate "message" when the player dies
@@ -194,7 +224,7 @@ while True:
     if (action == "quit" or action == "exit" or action == "bye" or action == "use"):
         break
     # set a default response
-    response = "I don't understand. Try Verb Noun Valid verbs are go, look, and take"
+    response = "I don't understand. Try Verb Noun Valid verbs are go, look, take, and use"
     # splits input into words (words are separated by spaces)
     words = action.split()
     # the game only undertands two word inputs
@@ -206,18 +236,22 @@ while True:
         # the verb is: go
         if verb == "go":
             # sets default response
-            response = "invalid exit."
-
+            response = "Invalid Exit."
             # check for valid exits in current room
             for i in range(len(currentRoom.exits)):
                 # a valid exit is found
                 if noun == currentRoom.exits[i]:
-                    # change the current room to the one that is
-                    # associated with the specified exit
-                    currentRoom = currentRoom.exitLocations[i]
-
-                    # set the response (success)
-                    response = "room changed"
+                    response = "Room changed"
+                    if noun == "window":
+                        for item in inventory:
+                            if item == "window_key":
+                                response = "You unlock the window and fall to your death."
+                                currentRoom = currentRoom.exitLocations[i]
+                                break
+                            else:
+                                response = "The window is locked" # was unable to make this response work
+                    else:
+                        currentRoom = currentRoom.exitLocations[i]
                     # no need to check any more exits
                     break
 
@@ -250,10 +284,21 @@ while True:
                     response = "item grabbed"
                     # no need to check for more grabbables
                     break
-       # elif verb == "use":
-        #    response = "Nothing happens"
-       #     for item in currentRoom.grabbables or inventory:
-       #         if currentRoom == "r4" and noun == "key"
+        elif verb == "use":
+            response = "Nothing happens"
+            # Sets a default response
+            for i in range(len(currentRoom.items)):
+                # a valid item is found
+                if noun == currentRoom.items[i]:
+                    response = currentRoom.usage[i]
+                    if noun =="puzzle_box":
+                        for item in inventory:
+                            if item == "key":
+                                response = "The box opens and you find another key"
+                                inventory.append("window_key")
+                                inventory.remove(item)
+                            break
+                    break
 
      # display the response
     print "\n{}".format(response)
