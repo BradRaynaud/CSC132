288467fa38c0a2e7a67f9a9ab184cba50870e1df
BradRaynaud <bradraynaud@gmail.com>
2017-02-10 01:50:09 -0600

    Class Game

--- Classwork/The_Game.py
+++ Classwork/The_Game.py
@@ -88,28 +88,45 @@ class Room(object):
     @grabbables.setter
     def grabbables(self, value):
         self._grabbables = value
-
+    # adds an exit to the room
+    # the exit is a string e.g. north
+    # the room is an instance of a room
     def addExit(self, exit, room):
+        # append the exit and room to the appropriate lists
         self._exits.append(exit)
         self._exitLocations.append(room)
-
+    # adds an item to the room
+    # the item is a string
+    # the desc is a description of the item
+    # the usage is a string with the response from the verb use
     def addItem(self, item, desc, usage): # added usage to addItem
+        # appends the item, description, and usage to the appropriate lists
         self._items.append(item)
         self._itemDescriptions.append(desc)
         self._usage.append(usage) # appends arg(usage) to the usage list for item
+    # adds a grabbable item to the room
+    # the item is a string
     def addGrabbable(self,item):
+        # append the item to the list
         self._grabbables.append(item)
-
+    # removes a grabbable item from the room
+    # the item is a string
     def delGrabbable(self,item):
+        # remove the item from the list
         self.grabbables.remove(item)
 
+    # Returns a string description of the room
     def __str__(self):
+        # first the room name
         s = "You are in {}.\n".format(self.name)
 
+        # next the items in the room
         s += "You see:  "
         for item in self.items:
             s += item + "  "
         s += "\n"
+
+        # next the exits from the room
         s += "Exits:  "
         for exit in self.exits:
             s += exit + "  "
@@ -117,48 +134,66 @@ class Room(object):
 ###################################################
 # creates the rooms
 
+
 def createRooms():
+    # r1 through r4 are the four rooms on the bottom floor
+    # r5 is the attic with the puzzle box and cake
+    # currentRoom is the room the player is currently in
+    # current room is global because it needs to be changed in the main part of the program
     global currentRoom
 
-    r1 = Room(" Room 1")
-    r2 = Room(" Room 2")
-    r3 = Room(" Room 3")
-    r4 = Room(" Room 4")
+    # gave the rooms names based off their contents
+    r1 = Room(" the Office")
+    r2 = Room(" the Living Room")
+    r3 = Room(" the Study")
+    r4 = Room(" the Kitchen")
     r5 = Room(" the Attic")
 
+    # adds exits to r1
     r1.addExit("east", r2)
     r1.addExit("south", r3)
+    # add grabbable to r1
     r1.addGrabbable("key")
+    # add items to r1
     r1.addItem("chair", "It is made of wicker and no one is sitting on it.", "I could sit in this, but I won't")
     r1.addItem("table", "it is made of oak. A golden key rests on it", "I can't use this")
 
+    # add exits to r2
     r2.addExit("stairs", r5)
     r2.addExit("west", r1)
     r2.addExit("south", r4)
+    # add items to r2
     r2.addItem("rug", "It is nice and Indian. It also needs to be vacuumed", "I can't use this, too dusty")
     r2.addItem("fireplace", "It is full of ashes.", "Don't want to burn the house down")
 
+    # add exits to r3
     r3.addExit("north", r1)
     r3.addExit("east", r4)
+    # add grabbable to r3
     r3.addGrabbable("book")
+    # add items to r3
     r3.addItem("bookshelves", "They are empty. Go figure.", "Books eww")
     r3.addItem("statue", "There is nothing special about it", "Nothing happens")
     r3.addItem("desk","The statue is resting on it. So is a book.", "I could study, But I won't")
 
+    # add exits to r4
     r4.addExit("north", r2)
     r4.addExit("west", r3)
     r4.addExit("window", None)  # DEATH
+    # add grabbable to r4
     r4.addGrabbable("6-pack")
+    # add item to r4
     r4.addItem("Brew_Rig", "Gourd is brewing some sort of oatmeal stout on the rig. A 6-pack is resting beside it.", "Better leaving brewing to Gourd")
 
+    # add exit to r5(attic)
     r5.addExit("stairs", r2)  # Adds an exit to the attic
+    # adds items to Exit
     r5.addItem("cake", "It looks tasty", "Made of chocolate, but tastes like lies")  # creates item: cake
     r5.addItem("puzzle_box", "Looks intricate but it does have a keyhole", "The key from downstairs might unlock this")  # Creates item: puzzle_box
-    # creates a base usage case if player does not have key in inventory
+
     currentRoom = r1
 
 # displays an appropriate "message" when the player dies
-# yes, this is intentionally obfuscated!
 def death():
     print " " * 17 + "u" * 7
     print " " * 13 + "u" * 2 + "$" * 11 + "u" * 2
@@ -227,7 +262,7 @@ while True:
     response = "I don't understand. Try Verb Noun Valid verbs are go, look, take, and use"
     # splits input into words (words are separated by spaces)
     words = action.split()
-    # the game only undertands two word inputs
+    # the game only understands two word inputs
     if len(words) == 2:
         # isolate the verb and noun
         verb = words[0]
@@ -241,16 +276,20 @@ while True:
             for i in range(len(currentRoom.exits)):
                 # a valid exit is found
                 if noun == currentRoom.exits[i]:
-                    response = "Room changed"
+                    response = "Room changed" # sets response
+                    # adds special case for window, player is unable to use the window as an exit until key is in inventory
                     if noun == "window":
+                        # checks inventory for item "window_key"
                         for item in inventory:
-                            if item == "window_key":
-                                response = "You unlock the window and fall to your death."
+                            if item == "window_key": # if window key is found window unlocks and player dies
+                                response = "You unlock the window and fall to your death." # sets response
+                                # changes the current room to the one associated with specified exit
                                 currentRoom = currentRoom.exitLocations[i]
                                 break
                             else:
                                 response = "The window is locked" # was unable to make this response work
                     else:
+                        # changes the current room to the one associated with specified exit
                         currentRoom = currentRoom.exitLocations[i]
                     # no need to check any more exits
                     break
@@ -258,7 +297,7 @@ while True:
         # The verb is: look
         elif verb == "look":
             # sets a default response
-            response = "I dont see that item"
+            response = "I don't see that item"
             # check for valid items in the current room
             for i in range(len(currentRoom.items)):
                 # a valid item is found
@@ -271,7 +310,7 @@ while True:
         # the verb is: take
         elif verb == "take":
             # sets a default response
-            response = "I dont see that item."
+            response = "I don't see that item."
             # checks for valid grabbable items in the current room
             for grabbable in currentRoom.grabbables:
                 # a valid grabbable was found
@@ -290,15 +329,16 @@ while True:
             for i in range(len(currentRoom.items)):
                 # a valid item is found
                 if noun == currentRoom.items[i]:
-                    response = currentRoom.usage[i]
-                    if noun =="puzzle_box":
+                    response = currentRoom.usage[i] # sets response to the items usage
+                    if noun =="puzzle_box": # adds special case for puzzle box
+                        # looks for item in inventory if item not found response defaults to "The key from downstairs might unlock this"
                         for item in inventory:
                             if item == "key":
-                                response = "The box opens and you find another key"
-                                inventory.append("window_key")
-                                inventory.remove(item)
+                                response = "The box opens and you find another key" # creates a new response
+                                inventory.append("window_key") # adds window_key to inventory
+                                inventory.remove(item) # removes key from inventory
                             break
                     break
 
-     # display the response
+    # displays the response
     print "\n{}".format(response)
