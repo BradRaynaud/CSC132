482da5ac07ca401ea2cfaa5b1196260dbaec7c5d
BradRaynaud <bradraynaud@gmail.com>
2017-04-19 22:56:32 -0500

    Points Plotted

--- paperPiano.py
+++ paperPiano.py
@@ -17,8 +17,9 @@ MIXER_BUFF = 1024
 # the note generator class
 class Note(pygame.mixer.Sound):
     # note that volume ranges from 0.0 to 1.0
-    def __init__(self, frequency, volume):
+    def __init__(self, frequency, volume, wavetype):
         self.frequency = frequency
+        self.wavetype = wavetype  # instance variable determining wave type
         # initialize the note using an array of samples
         pygame.mixer.Sound.__init__(self, buffer=self.build_samples())
         self.set_volume(volume)
@@ -33,10 +34,34 @@ class Note(pygame.mixer.Sound):
         samples = array("h", [0] * period)
         # generate the note's samples
         for t in range(period):
-            if (t < period / 2):
-                samples[t] = amplitude
-            else:
-                samples[t] = -amplitude
+            # implemented the square wavetype
+            if self.wavetype == "Square":  # yellow
+                if t < period / 2:
+                    samples[t] = amplitude
+                else:
+                    samples[t] = -amplitude
+
+            # implemented the triangle wavetype
+            # amplitude follows graph provided in the activity
+            elif self.wavetype == "Triangle":  # Green
+                if t >= period * 3 / 4:
+                    for n in range(-amplitude, 0, amplitude / 42):
+                        samples[t] = n
+                if t < period / 4:
+                    for n in range(0, amplitude, amplitude / 42):
+                        samples[t] = n
+                else:
+                    for n in range(amplitude, -amplitude, -amplitude / 85):
+                        samples[t] = n
+            # Implemented the sawtooth wavetype
+            # amplitude goes from 0 to max amp then from min amp to 0
+            elif self.wavetype == "Sawtooth":  # Blue
+                if t < period / 2:
+                    for n in range(0, amplitude, amplitude / 85):
+                        samples[t] = n
+                else:
+                    for n in range(-amplitude, 0, amplitude / 85):
+                        samples[t] = n
         return samples
 
 
@@ -50,6 +75,7 @@ def wait_for_note_start():
         sleep(0.01)
 
 
+# waits until a note is released
 def wait_for_note_stop(key):
     while (GPIO.input(key)):
         sleep(0.1)
@@ -66,22 +92,19 @@ GPIO.setmode(GPIO.BCM)
 
 # setup the pins and frequencies for the notes (C, D, E, F)
 keys = [26, 6, 12, 20]
-freqs = [261.6]
+waveType = ["Square", "Triangle", "Sawtooth"]  # list of wavetypes
 notes = []
 
 # setup the input pins
 GPIO.setup(keys, GPIO.IN, GPIO.PUD_DOWN)
 
 # create the actual notes
-for n in range(len(freqs)):
-    notes.append(Note(freqs[n], 1))
+for n in range(len(waveType)):
+    notes.append(Note(261.6, 1, waveType[n]))
 
 # the main part of the program
 print "Welcome to Paper Piano!"
 print "Press Ctrl+C to exit..."
-
-# detect when Ctrl+C is pressed so that we can reset the GPIO
-# pins
 
 try:
     while (True):
@@ -91,6 +114,6 @@ try:
         wait_for_note_stop(keys[key])
         notes[key].stop()
 
-
+# detect when Ctrl+C is pressed so that we can reset the GPIO
 except KeyboardInterrupt:
     GPIO.cleanup()
