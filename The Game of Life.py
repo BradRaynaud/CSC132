458478e2c57ad4b1a55ad01eb2b7029aa6c0b8d0
BradRaynaud <bradraynaud@gmail.com>
2017-04-28 15:07:22 -0500

    Surface Pro

--- The Game of Life.py
+++ The Game of Life.py
@@ -1,12 +1,73 @@
 # The game of life
 # Brad Raynaud
 # 4/28/2017
+#########################
+# Imports
 import sys
+import os
+from time import sleep
+#########################
 
-board = []
 
-for line in sys.stdin:
-    board.append([])
-    for c in line:
-            board
+def computeNextGen(board):
+    nextboard = copyBoard(board)
+    for row in range(1, size - 1):
+        for col in range(1, size - 1):
+            neighbors = countNeighbours(board, row, col)
+            if board[row][col] == "*"
+                if neightbours < 2 or neighbours > 3:
+                    nextboard[row][col] = " "
+            else:
+                if neighbors == 3:
+                    nextboard[row][col] = "*"
+    return nextboard
 
+
+def copyBoard(board):
+    nextboard = []
+    for row in board:
+        nextboard.append([])
+        for col in row:
+            nextboard[len(nextboard) - 1].append(col)
+    return nextboard
+
+
+def countNeighbours(board, row, col):
+    neighbors = 0
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if (not(i == 0 and j == 0)):
+                if board[row + i][col + i] == "*":
+                    neighbors += 1
+    return neighbors
+
+
+def printBoard(board):
+    print " ",
+    for i in range(1, 6):
+        print i % 10,
+    print
+    for row in range(1, 6):
+        print row % 10,
+        for col in range(1, 6):
+            print board[row][col],
+        print
+
+
+board = [[" ", '*', '*', '*', ' ', '*', " "],
+         [" ", '*', ' ', ' ', ' ', ' ', " "],
+         [" ", ' ', ' ', ' ', '*', '*', " "],
+         [" ", ' ', '*', '*', ' ', '*', " "],
+         [" ", '*', ' ', '*', ' ', '*', " "]]
+
+NUM_GENS = 25
+os.system("clear")
+print "Gen 0"
+printBoard(board)
+for i in range(NUM_GENS):
+    sleep(.5)
+    os.system("clear")
+
+    print "Gen {}".format(i)
+    board = computeNextGen(board)
+    printBoard(board)
