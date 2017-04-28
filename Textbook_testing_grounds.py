78066700809e141605b79bc7b0feff1f4db14d54
BradRaynaud <bradraynaud@gmail.com>
2017-04-28 14:23:42 -0500

    Surface Pro

--- Textbook_testing_grounds.py
+++ Textbook_testing_grounds.py
@@ -1,25 +1,5 @@
-def foo(n):
-    if n == 1:
-        return 1
-    else:
-        x = foo(n-1)
-        x = x + 3*n -2
-        return x
 
-print foo(2)
 
-x = 0
-terms = []
-for i in range(1,8):
-    if i == 1:
-        x += 1
-        terms.append(x)
-    else:
-        x = x+3*i-2
-        terms.append(x)
+dict = {"key":["value1", "value2"]}
 
-print terms
-print int("0xFF0000",0)
-print int("0x00FF00",0)
-print int("0x0000FF",0)
-print hex(255)
+print dict["key"][0]
---
+++ The Game of Life.py
@@ -0,0 +1,12 @@
+# The game of life
+# Brad Raynaud
+# 4/28/2017
+import sys
+
+board = []
+
+for line in sys.stdin:
+    board.append([])
+    for c in line:
+            board
+
